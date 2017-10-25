c61eb7df1d64fa662b50f4b9d7ec530955594452
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-10-25 14:31:17 -0700

    Add function to add shade class to the circle div if the attribute matches. #1

--- public/main.js
+++ public/main.js
@@ -17,85 +17,110 @@ const cycle = {
 slides()
   .then(documents => (carousel.slides = documents))
   .then(documents => (cycle.slides = documents))
+  .then(() => slides())
+
+const $circle1 = document.getElementById('circle1')
+const $circle2 = document.getElementById('circle2')
+const $circle3 = document.getElementById('circle3')
+
+const addShade = () => {
+  if (carousel.current === 1) {
+    $circle1.classList.add('shade')
+    $circle3.classList.remove('shade')
+  }
+  else if (carousel.current === 2)
+  {
+    $circle2.classList.add('shade')
+    $circle1.classList.remove('shade')
+  }
+  else if (carousel.current === 0)
+  {
+    $circle3.classList.add('shade')
+    $circle2.classList.remove('shade')
+  }
+}
+
+const addShadeLeft = () => {
+  if (cycle.left === 1) {
+    $circle1.classList.add('shade')
+    $circle3.classList.remove('shade')
+  }
+  else if (cycle.left === 2)
+  {
+    $circle2.classList.add('shade')
+    $circle1.classList.remove('shade')
+  }
+  else if (cycle.left === 0)
+  {
+    $circle3.classList.add('shade')
+    $circle2.classList.remove('shade')
+  }
+}
+
+const addShadeRight = () => {
+  if (cycle.right === 1) {
+    $circle1.classList.add('shade')
+    $circle3.classList.remove('shade')
+  }
+  else if (cycle.right === 2)
+  {
+    $circle2.classList.add('shade')
+    $circle1.classList.remove('shade')
+  }
+  else if (cycle.right === 0)
+  {
+    $circle3.classList.add('shade')
+    $circle2.classList.remove('shade')
+  }
+}
 
 const $leftArrow = document.getElementById('arrow-left')
 const $rightArrow = document.getElementById('arrow-right')
 
-$leftArrow.addEventListener('click', () => {
+$rightArrow.addEventListener('click', () => {
   document
     .querySelector('.slide')
     .setAttribute('src', `${cycle.slides[cycle.right].url}`)
 
     if (cycle.right < cycle.slides.length - 1) {
       cycle.right++
+      addShadeRight()
     }
     else {
       cycle.right = 0
+      addShadeRight()
     }
 })
 
-$rightArrow.addEventListener('click', () => {
+$leftArrow.addEventListener('click', () => {
   document
     .querySelector('.slide')
     .setAttribute('src', `${cycle.slides[cycle.left].url}`)
 
     if (cycle.left < cycle.slides.length - 1) {
       cycle.left++
+      addShadeLeft()
     }
     else {
       cycle.left = 0
+      addShadeLeft()
     }
 })
 
-setInterval(function() {
+const switchSlide = () => {
   document
     .querySelector('.slide')
     .setAttribute('src', `${carousel.slides[carousel.current].url}`)
+}
 
+setInterval(function() {
     if (carousel.current < carousel.slides.length - 1) {
       carousel.current++
+      addShade()
     }
     else {
       carousel.current = 0
+      addShade()
     }
-}, 3000)
-
-// const renderPhoto = (image) => {
-//   console.log(image)
-//   const $box = document.createElement('div')
-//   const $photo = document.createElement('img')
-//   $photo.src = image.url
-//   $photo.setAttribute('class', 'hidden')
-//   $box.appendChild($photo)
-//   return $box
-// }
-//
-// const fetchPhoto = (params) => {
-//   return fetch('/carouselPhotos/')
-//     .then(res => res.json())
-// }
-//
-// // const displayImage = () => {
-// //   fetchPhoto()
-// //     .then(renderPhoto)
-// //     .then(data => {
-// //       document.body.appendChild(data)
-// //     })
-// // }
-//
-// const displayImage = () => {
-//   fetchPhoto()
-//     .then(data => data.map(renderPhoto))
-//     .then((data) => {
-//       data.forEach(element => {
-//         document.body.appendChild(element)
-//       })
-//     })
-// }
-//
-// displayImage()
-//
-// setInterval(() => {
-//   const $photo = document.getElementsByClassName('hidden')
-//   $photo.classList.remove('hidden')
-// }, 1000)
+    switchSlide()
+}, 1000)
